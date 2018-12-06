6cb5b12dbc127d6a5f20d12e2f4f9f5e1d21561c
ira-gl <ilm150130@utdallas.edu>
2018-12-06 15:25:49 -0600

    Working admin view

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -9,6 +9,7 @@ const routes: Routes = [
         children: [
             { path: '', redirectTo: 'dashboard', pathMatch: 'prefix' },
             { path: 'charts', loadChildren: './charts/charts.module#ChartsModule' },
+            { path: 'admin-chart', loadChildren: './admin-chart/charts.module#ChartsModule' },
             { path: 'bs-element', loadChildren: './bs-element/bs-element.module#BsElementModule' },
             { path: 'components', loadChildren: './bs-component/bs-component.module#BsComponentModule' },
             { path: 'blank-page', loadChildren: './blank-page/blank-page.module#BlankPageModule' }
